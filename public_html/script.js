f60484038b7fbe27fc14d242390a2d29eca1e1f1
Nicholas Mostacero <nickmostacero@gmail.com>
2015-01-09 11:09:59 -0800

    commit

--- public_html/script.js
+++ public_html/script.js
@@ -5,8 +5,31 @@
  */
 
 $("document").ready(function(){
-     $(".nicksclass").css("background-color", "yellow");
-
+     $('.nicksclass').css('background-color', 'yellow');
+     
+     $('.2').css({'visibility': 'hidden', 'position': 'absolute'});
+     
+     $('div:last p:nth-child(3)').css('background-color', 'pink');
+     
+     $('p:last').css('color', 'red');
+     
+     $('p:gt(4)').css('border-style', 'solid');
+     
+     $('p:first').css({'background-color':'cyan', 'color': 'white'});
+     
+     $("h1").bind('mousecover', mouseOverMe).bind('mouseout', mouseOutMe);
+     
+     $('h1').bind('click', mouseClick);
+     
+     
 });
 
+
+function mouseOverMe(){
+    $("h1").html("Nick is really cool");
+}
+
+function mouseOutMe(){
+    $('h1').html('Nick is pretty cool');
+}
 
